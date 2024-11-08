b2c10d7568b98414093ae38cd1a295cafb0b5196
Martini0717 <mkuklinska2@gmail.com>
2024-11-08 13:19:30 +0100

    Update zadanie6.py

--- zadanie6.py
+++ zadanie6.py
@@ -1,1 +1,3 @@
-#Pętla while powtarza coś dopóki warunek nie zostanie spełniony, a for powtarza coś dla kilku rzeczy, elementów, albo daną ilość razy. To jak jakby powiedzieć gotuj mleko, dopóki nie wykipi (while), 
+#Pętla while powtarza coś dopóki warunek nie zostanie spełniony
+#To jak jakby powiedzieć gotuj mleko, dopóki nie wykipi (while)  
+#a for powtarza coś dla kilku elementów, albo daną ilość razy. 
